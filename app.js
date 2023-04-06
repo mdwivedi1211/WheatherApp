b9f38edf2334ab50140f529ae6fa7e5c609513fe
ManojDwivedi <mdwivedi1211@gmail.com>
2023-04-06 14:36:08 +0530

    initail commit

---
+++ app.js
@@ -0,0 +1,19 @@
+function getWeather() {
+  const city = document.getElementById("city").value;
+  const apiKey = "your_api_key"; // Replace with your API key
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = data.weather[0].main;
+      const temp = Math.round(data.main.temp - 273.15);
+      const result = document.getElementById("result");
+      result.innerHTML = `Weather: ${weather}<br>Temperature: ${temp}°C`;
+    })
+    .catch((error) => {
+      console.error(error);
+      const result = document.getElementById("result");
+      result.innerHTML = `Error: ${error.message}`;
+    });
+}
