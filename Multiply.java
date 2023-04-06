36235fbed6ba147734f160ef2c8bbce6b9154c0b
ManojDwivedi <mdwivedi1211@gmail.com>
2023-04-06 15:03:06 +0530

    Added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+class Multply
+{
+    public static void main(String [] args)
+    {
+        int a=10;
+        int b=20;
+        System.out.println("Multiplication of a and b is"+(a*b));
+    }
+}
